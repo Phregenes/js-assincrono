82637f149935fdb97dc96b5d6ff63c6e95111120
Pedro Regenes <phregenes@gmail.com>
2019-10-29 03:13:20 -0300

    Projeto Pronto

--- todos.js
+++ todos.js
@@ -7,3 +7,46 @@ var todos = [
     'estudar js',
     'ola mundo'
 ];
+
+function renderTodos() {
+    listElement.innerHTML = '';
+
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement('a');
+
+        linkElement.setAttribute('href' , '#');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick' , 'deleteTodo(' + pos + ')');
+
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+
+        listElement.appendChild(todoElement);
+    }
+}
+renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+    renderTodos();
+    
+
+}
